47ed5d5c37cf7f34f5fdc51a37e8197688adaf9b
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-17 23:05:35 +0330

    Create home and show-picture pages

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from "react";
+import StackGrid, {transitions} from "react-stack-grid";
+import PictureCard from "../../components/PictureCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import {fetchPictures} from "../../api";
+import {useNavigate,Outlet} from "react-router-dom";
+
+const {scaleDown} = transitions;
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    const [pictures, setPictures] = useState([]);
+    const [columnWidth, setColumnWidth] = useState(300);
+
+    useEffect(() => {
+        specifyColumnWidth()
+        fetchPictures(25, pictures.length).then(result => {
+            let copyOfPictures = [...pictures];
+            copyOfPictures = [...copyOfPictures, ...result];
+            setPictures(copyOfPictures);
+        }).catch(error => console.log(error));
+    }, []);
+
+    window.addEventListener('resize', () => {
+        specifyColumnWidth();
+    });
+
+    const specifyColumnWidth = () => {
+        if (window.innerWidth > 1300) setColumnWidth(300);
+        if (window.innerWidth < 1300 && window.innerWidth > 1050) setColumnWidth(230);
+        if (window.innerWidth < 1050 && window.innerWidth > 840) setColumnWidth(180);
+        if (window.innerWidth < 840 && window.innerWidth > 650) setColumnWidth(140);
+        if (window.innerWidth < 650 && window.innerWidth > 550) setColumnWidth(250);
+        if (window.innerWidth < 550 && window.innerWidth > 440) setColumnWidth(200);
+        if (window.innerWidth < 440 && window.innerWidth > 360) setColumnWidth(160);
+        if (window.innerWidth < 360 && window.innerWidth > 260) setColumnWidth(120);
+    }
+
+    const fetchData = () => {
+        fetchPictures(15, pictures.length).then(result => {
+            let copyOfPictures = [...pictures];
+            copyOfPictures = [...copyOfPictures, ...result];
+            setPictures(copyOfPictures);
+        }).catch(error => console.log(error));
+    }
+
+    const ShowPicture = (picture) => {
+        navigate(`/${picture.id}`, {
+            state: {cachedPicture: picture}
+        })
+    }
+
+    return <React.Fragment>
+        <InfiniteScroll
+            dataLength={pictures.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<LoadingSpinner/>}
+            scrollThreshold={1}
+            endMessage={
+                <p style={{textAlign: 'center'}}>
+                    <b>Yay! You have seen it all</b>
+                </p>
+            }
+            className="infinite-scroll-container"
+        >
+            <StackGrid
+                columnWidth={columnWidth}
+                appear={scaleDown.appear}
+                appeared={scaleDown.appeared}
+                enter={scaleDown.enter}
+                entered={scaleDown.entered}
+                leaved={scaleDown.leaved}
+                itemComponent="span"
+                gutterWidth={18}
+                gutterHeight={18}
+            >
+                {
+                    pictures.map(picture => (
+                        <PictureCard
+                            key={picture.id}
+                            title={picture.title}
+                            imageSrc={picture.picture}
+                            onClick={() => ShowPicture(picture)}
+                        />
+                    ))
+                }
+            </StackGrid>
+        </InfiniteScroll>
+        <Outlet />
+    </React.Fragment>;
+}
+
+export default HomePage;
