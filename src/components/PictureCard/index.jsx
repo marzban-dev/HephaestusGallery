c50b8d38c0aa26bb31e8ed4667b9d747566e727e
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-17 23:06:36 +0330

    Update PictureCard

--- src/components/PictureCard/index.jsx
+++ src/components/PictureCard/index.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
-const PictureCard = ({imageSrc, title}) => {
-    return <img src={imageSrc} alt={title} style={{
+const PictureCard = ({imageSrc, title,onClick}) => {
+
+    const pictureCardImgStyle = {
         width: "100%",
-        height: "auto",
-        borderRadius:"10px"
-    }}/>
+        borderRadius: "10px",
+        transform: "scale(1)",
+        transition: "transform 0.1s",
+        cursor: "pointer",
+        userSelect: "none"
+    };
+
+    const onMouseDown = (e) => e.target.style.transform = "scale(0.97)";
+    const onMouseUp = (e) => e.target.style.transform = "scale(1)";
+    const onMouseEnter = (e) => e.target.style.transform = "scale(1.04)";
+    const onMouseLeave = (e) => e.target.style.transform = "scale(1)";
+
+    return <img
+        src={imageSrc}
+        alt={title}
+        style={pictureCardImgStyle}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}
+    />
 }
 
 export default PictureCard;
