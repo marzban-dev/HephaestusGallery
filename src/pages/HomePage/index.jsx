173d8c18b8a6a41c4ceffe388ef54b07c39b4031
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-19 01:47:50 +0330

    Update HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect} from "react";
 import {fetchPictures} from "api";
 import {Outlet} from "react-router-dom";
 import ShowPictures from "components/ShowPictures";
+import SearchBox from "components/SearchBox";
+import {TemplateContext} from "context/PicturesContext";
 
 const HomePage = () => {
-
-    const [pictures, setPictures] = useState([]);
+    const {pictures, setPictures, filter, search} = useContext(TemplateContext);
 
     useEffect(() => {
-        fetchPictures(25, pictures.length).then(result => {
+        fetchPictures(25, pictures.length).then(({result, count}) => {
             let copyOfPictures = [...pictures];
             copyOfPictures = [...copyOfPictures, ...result];
             setPictures(copyOfPictures);
@@ -16,15 +17,16 @@ const HomePage = () => {
     }, []);
 
     const fetchData = () => {
-        fetchPictures(15, pictures.length).then(result => {
+        fetchPictures(15, pictures.length, {filter, search}).then(({result, count}) => {
             let copyOfPictures = [...pictures];
             copyOfPictures = [...copyOfPictures, ...result];
             setPictures(copyOfPictures);
-        }).catch(error => console.log(error));
-    }
+        }
+    ).catch(error => console.log(error))}
 
     return (
         <React.Fragment>
+            <SearchBox/>
             <ShowPictures pictures={pictures} fetchData={fetchData}/>
             <Outlet/>
         </React.Fragment>
