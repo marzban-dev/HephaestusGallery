3ea84492e89bd40a93136554abb97d6ef19645f2
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-19 01:44:52 +0330

    Add PicturesContext provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import {BrowserRouter} from "react-router-dom";
+import PicturesContext from "context/PicturesContext";
 
 ReactDOM.render(
     <BrowserRouter>
         <React.StrictMode>
-            <App/>
+            <PicturesContext>
+                <App/>
+            </PicturesContext>
         </React.StrictMode>
     </BrowserRouter>,
     document.getElementById('root')
