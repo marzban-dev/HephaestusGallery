26210fd4139827ae16ec89cd82cfe7d7a8fbe4b2
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-19 01:42:17 +0330

    Add filter and search to api

--- src/api.js
+++ src/api.js
@@ -1,16 +1,24 @@
 import axios from "axios";
 
-export const fetchPictures = async (limit, offset) => {
+export const fetchPictures = async (limit, offset, searchObj) => {
+
+    const filterObject = {};
+
+    if (searchObj && searchObj.search.length !== 0) {
+        filterObject[`${searchObj.filter.toLowerCase()}__contains`] = searchObj.search;
+    }
+
     try {
         const pictures = await axios.get("https://onlineartdatabase.pythonanywhere.com/", {
             params: {
                 format: "json",
                 limit,
-                offset
+                offset,
+                ...filterObject
             }
         });
 
-        return pictures.data.results;
+        return {result: pictures.data.results, count: pictures.data.count};
     } catch (error) {
         console.log(error);
     }
