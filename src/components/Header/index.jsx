f4419fe3809414eec4516b0c0e320c73cf19f143
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-21 02:29:19 +0330

    Update app title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header-gradient"></div>
-            <h1>Picture Gallery</h1>
+            <h1>Hephaestus</h1>
         </header>
     )
 }
