320de34f5b87b0821013013cc1a3eb375b14d198
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-20 15:40:19 +0330

    Fix request problem

--- src/components/SearchBox/components/SearchInput/index.jsx
+++ src/components/SearchBox/components/SearchInput/index.jsx
@@ -1,38 +1,46 @@
-import React, {useRef, useState, useContext} from "react";
+import React, {useState, useContext, useRef} from "react";
 import {TemplateContext} from "context/PicturesContext";
-import "./searchInput.css";
 import {fetchPictures} from "api";
+import "./searchInput.css";
 
 const SearchInput = () => {
     const {setPictures, filter, search, setSearch, setCount} = useContext(TemplateContext);
     const [timerID, setTimerId] = useState(null);
-    const searchTextRef = useRef(search);
+    const searchText = useRef(search);
 
     const fetchData = () => {
-        fetchPictures(25, 0, {filter, search}).then(({result, count}) => {
-            setPictures(result);
-            setCount(count)
-        }).catch(error => console.log(error));
+        if (search.length !== 0) {
+            fetchPictures(25, 0, {filter, search:searchText.current}).then(({result, count}) => {
+                setPictures(result);
+                setCount(count);
+            }).catch(error => console.log(error));
+        }else{
+            fetchPictures(25, 0).then(({result, count}) => {
+                setPictures(result);
+                setCount(count)
+            }).catch(error => console.log(error));
+        }
     }
 
     const onSearchInputChanged = (e) => {
-        const inputValue = e.target.value;
+        setSearch(e.target.value)
+        searchText.current = e.target.value;
 
-        setSearch(inputValue)
-        searchTextRef.current = inputValue;
+        if (timerID) clearTimeout(timerID);
 
-        if (inputValue.length !== 0) {
-            if (timerID) clearTimeout(timerID);
-
-            setTimerId(setTimeout(() => {
-                fetchData();
-            }, 1200));
-        }
+        setTimerId(setTimeout(() => {
+            fetchData();
+        }, 1200));
     }
 
     return (
         <div className="search-input">
-            <input type="text" placeholder="Write your mind here" value={search} onChange={onSearchInputChanged}/>
+            <input
+                type="text"
+                placeholder={filter === "Year" ? "e.g : 1990-98" : "Write your mind here"}
+                value={search}
+                onChange={onSearchInputChanged}
+            />
         </div>
     );
 }
