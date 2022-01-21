749680272a046a153fb75cd131eb215705b8b709
X-Dark-Coder <marzban.dev@gmail.com>
2022-01-21 22:54:50 +0330

    Create header about text Create header github accounts avatar

--- src/components/Header/SocialMedia/index.jsx
+++ src/components/Header/SocialMedia/index.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import TwitterIcon from "assets/twitter-svgrepo-com.svg";
-import GithubIcon from "assets/github-svgrepo-com.svg";
 import ApiIcon from "assets/api-svgrepo-com.svg";
 import "./socialMedia.css";
 
 const SocialMedia = () => {
+  const socialMediaItems = [
+    {
+      title: "Twitter",
+      img: TwitterIcon,
+      w: "30px",
+      link: "https://twitter.com/hephaestus_gall",
+    },
+    {
+      title: "Api",
+      img: ApiIcon,
+      w: "30px",
+      link: "https://onlineartdatabase.pythonanywhere.com/",
+    },
+  ];
 
-    const socialMediaItems = [
-        {title: "Twitter", img: TwitterIcon, w: "30px", link: "https://twitter.com/hephaestus_gall"},
-        {title: "Github", img: GithubIcon, w: "35px", link: "https://github.com/X-Dark-Coder"},
-        {title: "Api", img: ApiIcon, w: "30px", link: "https://onlineartdatabase.pythonanywhere.com/"}
-    ];
+  const renderSocialMediaItems = () => {
+    return socialMediaItems.map((item) => {
+      return (
+        <a href={item.link} className="social-media-list-item">
+          <img src={item.img} alt={item.title} width={item.w} />
+        </a>
+      );
+    });
+  };
 
-    const renderSocialMediaItems = () => {
-        return socialMediaItems.map(item => {
-            return (
-                <a href={item.link} className="social-media-list-item">
-                    <img src={item.img} alt={item.title} width={item.w}/>
-                </a>
-            );
-        });
-    }
-
-    return (<div className="social-media">
-        <div className="social-media-github">
-            {/*<img src={GithubIcon} alt="github"/>*/}
-        </div>
-        <div className="social-media-list">
-            {renderSocialMediaItems()}
-        </div>
-    </div>);
-}
+  return (
+    <div className="social-media">
+      <div className="social-media-github">
+        <a href="https://github.com/X-Dark-Coder" className="social-media-github-item" title="Mansour">
+          <img src="https://avatars.githubusercontent.com/u/84951627?v=4" alt="X-Dark-Coder Github" />
+        </a>
+        <a href="https://github.com/nimiology" className="social-media-github-item" title="Nima">
+          <img src="https://avatars.githubusercontent.com/u/73313947?v=4" alt="nimiology Github" />
+        </a>
+      </div>
+      <div className="social-media-list">{renderSocialMediaItems()}</div>
+    </div>
+  );
+};
 
 export default SocialMedia;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,14 @@ const Header = () => {
         <header className="header">
             <SocialMedia />
             <div className="header-gradient"></div>
-            <h1>Hephaestus Gallery</h1>
+            <h1 className="header-title">Hephaestus Gallery</h1>
+            <div className="header-about">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magni nam, eligendi pariatur aliquid quam dolorum ratione
+                    amet minus magnam, veniam laborum, deserunt vero modi!
+                </p>
+            </div>
         </header>
     )
 }
